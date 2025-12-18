b2f67e846d1fd32a8ecd6506d39cd9b62a093715
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2025-12-18 12:37:53 +0100

    feat: añadir modelo de la aplicaion a inicio publico

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -1,3 +1,7 @@
 <div class="hero-text">
     <h1>Bienvenido a Inicio Publico</h1>
+    <div class="imagenesInicio">
+        <img src="./webroot/images/modelo.png" alt="">
+        <img src="./webroot/images/diagramaDeClases.png" alt="">
+    </div>
 </div>
