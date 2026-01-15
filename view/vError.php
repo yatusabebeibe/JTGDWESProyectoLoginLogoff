cda270470aedecf81c942b1087d6ca53b64c091c
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-15 13:36:51 +0100

    style: mejorar estilos de la vista del error

--- view/vError.php
+++ view/vError.php
@@ -1,10 +1,18 @@
 <div class="hero-text">
     <h1>Ha habido un error inesperado</h1>
+    <style>
+        pre {margin: 5px; margin-bottom: 15px;}
+    </style>
     <br>
-    <h2>Código de error: <?= $avError['codError'] ?></h2>
-    <h3>Descripción del error: <?= $avError['descError'] ?></h3>
-    <h3>Archivo donde se ha producido el error: <?= $avError['archivoError'] ?></h3>
-    <h3>Línea del archivo donde se ha producido el error: <?= $avError['lineaError'] ?></h3>
+    <h2>Código de error:</h2>
+    <pre><?= $avError['codError'] ?></pre>
+    <h3>Descripción del error:</h3>
+    <pre><?= $avError['descError'] ?></pre>
+    <h3>Archivo donde se ha producido el error:</h3>
+    <pre><?= $avError['archivoError'] ?></pre>
+    <h3>Línea del archivo donde se ha producido el error:</h3>
+    <pre><?= $avError['lineaError'] ?></pre>
+    <br>
     <form action=<?php echo $_SERVER["PHP_SELF"];?> method="post">
         <div><input type="submit" value="Volver" name="volver"></div>
     </form>
